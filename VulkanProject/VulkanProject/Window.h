4571e4988407143be6ca7de63c19d07dbfa2ba9c
Liam-Maclean <Liamm107@hotmail.co.uk>
2018-09-18 12:20:33 +0100

    Uni commit
    Change to uni computers

--- VulkanProject/VulkanProject/Window.h
+++ VulkanProject/VulkanProject/Window.h
@@ -26,11 +26,9 @@ struct SwapChainSupportDetails {
 
 struct Vertex {
 	glm::vec3 pos;
+	glm::vec3 color;
 	glm::vec2 texCoord;
 	glm::vec3 normal;
-	glm::vec3 color;
-
-
 
 	static VkVertexInputBindingDescription GetBindingDescription()
 	{
@@ -45,6 +43,7 @@ struct Vertex {
 	static std::array<VkVertexInputAttributeDescription, 4> GetAttributeDescriptions()
 	{
 		std::array <VkVertexInputAttributeDescription, 4> attribute_descriptions = {};
+
 		attribute_descriptions[0].binding = 0;
 		attribute_descriptions[0].location = 0;
 		attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
@@ -52,18 +51,18 @@ struct Vertex {
 
 		attribute_descriptions[1].binding = 0;
 		attribute_descriptions[1].location = 1;
-		attribute_descriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
-		attribute_descriptions[1].offset = offsetof(Vertex, texCoord);
+		attribute_descriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attribute_descriptions[1].offset = offsetof(Vertex, color);
 
 		attribute_descriptions[2].binding = 0;
 		attribute_descriptions[2].location = 2;
-		attribute_descriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attribute_descriptions[2].offset = offsetof(Vertex, normal);
+		attribute_descriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+		attribute_descriptions[2].offset = offsetof(Vertex, texCoord);
 
 		attribute_descriptions[3].binding = 0;
 		attribute_descriptions[3].location = 3;
 		attribute_descriptions[3].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attribute_descriptions[3].offset = offsetof(Vertex, color);
+		attribute_descriptions[3].offset = offsetof(Vertex, normal);
 
 		return attribute_descriptions;
 	}
